769512ad0926ce84d6110f907bd442957f780e60
frankvp <90110502+frankvp11@users.noreply.github.com>
2023-04-28 13:34:36 -0400

    Add files via upload

--- temp.py
+++ temp.py
@@ -1,28 +1,6 @@
-import tensorflow as tf
-from tensorflow import keras
 
 
-from tensorflow.keras.layers import Dense, Activation
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.metrics import categorical_crossentropy
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import Model
-from tensorflow.keras.applications import imagenet_utils
-import itertools
 import os
-import shutil
-import random
-
-mobile = tf.keras.applications.mobilenet.MobileNet()
-def prepare_image(file):
-    img_path = ''
-    img = image.load_img(img_path + file, target_size=(224, 224))
-    img_array = image.img_to_array(img)
-    img_array_expanded_dims = np.expand_dims(img_array, axis=0)
-    return tf.keras.applications.mobilenet.preprocess_input(img_array_expanded_dims)
-  
-preprocessed_image = prepare_image('/content/df42701e.jpg')
-predictions = mobile.predict(preprocessed_image)
-results = imagenet_utils.decode_predictions(predictions)
-print(results)
+import time
+time.sleep(15)
+os.system("sudo docker run -it --rm -e DISPLAY=$DISPLAY --device /dev/video0:/dev/video0 -v /tmp/argus_socket:/tmp/argus_socket -v /tmp/.X11-unix:/tmp/.X11-unix --network host nvcr.io/nvidia/l4t-tensorflow:r32.7.1-tf2.7-py3")
